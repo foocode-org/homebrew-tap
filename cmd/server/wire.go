7c5a5da44c82941e40f926fd447ac4e0fa2c40d8
Noushad <npm@veracode.com>
2024-03-22 20:40:45 +0530

    homebrew test v5

---
+++ cmd/server/wire.go
@@ -0,0 +1,42 @@
+//go:build wireinject
+// +build wireinject
+
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"helloworld/internal/handler"
+	"helloworld/internal/repository"
+	"helloworld/internal/server"
+	"helloworld/internal/service"
+	"helloworld/pkg/log"
+	"github.com/google/wire"
+	"github.com/spf13/viper"
+)
+
+var ServerSet = wire.NewSet(server.NewServerHTTP)
+
+var RepositorySet = wire.NewSet(
+	repository.NewDb,
+	repository.NewRepository,
+	repository.NewUserRepository,
+)
+
+var ServiceSet = wire.NewSet(
+	service.NewService,
+	service.NewUserService,
+)
+
+var HandlerSet = wire.NewSet(
+	handler.NewHandler,
+	handler.NewUserHandler,
+)
+
+func newApp(*viper.Viper, *log.Logger) (*gin.Engine, func(), error) {
+	panic(wire.Build(
+		ServerSet,
+		RepositorySet,
+		ServiceSet,
+		HandlerSet,
+	))
+}
---
+++ internal/server/http.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"helloworld/internal/handler"
+	"helloworld/internal/middleware"
+	"helloworld/pkg/helper/resp"
+	"helloworld/pkg/log"
+)
+
+func NewServerHTTP(
+	logger *log.Logger,
+	userHandler handler.UserHandler,
+) *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	r.Use(
+		middleware.CORSMiddleware(),
+	)
+	r.GET("/", func(ctx *gin.Context) {
+		resp.HandleSuccess(ctx, map[string]interface{}{
+			"say": "Hi Nunu!",
+		})
+	})
+	r.GET("/user", userHandler.GetUserById)
+
+	return r
+}
