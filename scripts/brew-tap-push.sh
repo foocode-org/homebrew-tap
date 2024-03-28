6e5cd687bd39c7b6d456117fa2d6471a3872f64e
Noushad <npm@veracode.com>
2024-03-28 21:51:39 +0530

    test: shell script for brew tap v1.5

--- scripts/brew-tap-push.sh
+++ scripts/brew-tap-push.sh
@@ -7,10 +7,8 @@ else
   VERSION=$CI_COMMIT_TAG
 fi
 
-git config user.email "test@example.com"
-git config user.name "Noushad"
-git config user.email
-git config user.name
+git config --global user.email "test@example.com"
+git config --global user.name "Noushad"
 git clone https://github.com/foocode123/homebrew-tools.git
 cd homebrew-tools/Formula
 cp -f ../../updated_formula.rb foocode-cli.rb
@@ -19,6 +17,8 @@ git commit -m "Brew formula update for foocode-cli version $VERSION"
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/foocode123/homebrew-tools.git
 git push origin main
 
+git config --unset-all user.email
+git config --unset-all user.name
 cd ../../
 rm -rf ./updated_formula.rb
 rm -rf ./homebrew-tools
