95871d3f7d23206bcca85ceafb82fe6f01f4a2bd
Noushad <npm@veracode.com>
2024-03-28 21:42:45 +0530

    test: shell script for brew tap v1.4

--- scripts/brew-tap-push.sh
+++ scripts/brew-tap-push.sh
@@ -9,11 +9,13 @@ fi
 
 git config user.email "test@example.com"
 git config user.name "Noushad"
+git config user.email
+git config user.name
 git clone https://github.com/foocode123/homebrew-tools.git
 cd homebrew-tools/Formula
-cp -f ../../updated_formula.rb veracode-cli.rb
+cp -f ../../updated_formula.rb foocode-cli.rb
 git add .
-git commit -m "Brew formula update for veracode-cli version $VERSION"
+git commit -m "Brew formula update for foocode-cli version $VERSION"
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/foocode123/homebrew-tools.git
 git push origin main
 
