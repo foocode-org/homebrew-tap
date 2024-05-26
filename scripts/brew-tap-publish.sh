0c06e724689fd815b43a4635d5d2035bc9cab7a4
Noushad <npm@veracode.com>
2024-05-26 11:08:53 +0530

    brew release update v1.9

--- scripts/brew-tap-publish.sh
+++ scripts/brew-tap-publish.sh
@@ -2,6 +2,8 @@ ls ./
 
 git config --global user.email ${GITHUB_USER_EMAIL}
 git config --global user.name ${GITHUB_USER_NAME}
+
+# clone the brew-tap repository
 git clone https://github.com/foocode123/homebrew-tools.git
 cd homebrew-tools/Formula
 cp -f ./dist/homebrew/helloworld.rb /homebrew-tools/Formula/
