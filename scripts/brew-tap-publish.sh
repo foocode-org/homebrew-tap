67bd63fcb7ec23c790f3766cf8aee77d1fb57d91
Noushad <npm@veracode.com>
2024-06-04 15:13:38 +0530

    brew release update v2.4

--- scripts/brew-tap-publish.sh
+++ scripts/brew-tap-publish.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+if [[ ${CI_COMMIT_TAG:0:1} == "v" ]]
+then
+  VERSION=${CI_COMMIT_TAG:1}
+else
+  VERSION=$CI_COMMIT_TAG
+fi
+
 git config --global user.email ${GITHUB_USER_EMAIL}
 git config --global user.name ${GITHUB_USER_NAME}
 
 # clone the brew-tap repository
 git clone https://github.com/Noush-012/homebrew-tools.git
 cd homebrew-tools/Formula
-cp -f ../../dist/homebrew/Formula/helloworld.rb .
+cp -f ../../dist/homebrew/Formula/helloworld.rb helloworld.rb
 
 git add .
-git commit -m "Brew formula update for foocode-cli version $VERSION"
+git commit -m "Brew formula update for helloworld-cli version $VERSION"
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/Noush-012/homebrew-tools.git
 git push origin main
 
